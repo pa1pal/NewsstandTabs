6f44e9bb94fa50b4bebd62ded16fe107a0d97129
pa1pal <pawanpal004@gmail.com>
2017-10-27 23:48:38 +0530

    added custom outline

--- tabs/src/main/java/pa1pal/newsstand/tabs/Tabs.java
+++ tabs/src/main/java/pa1pal/newsstand/tabs/Tabs.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Outline;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewOutlineProvider;
 
 /**
  * Created by pa1pal on 27/10/17.
@@ -19,7 +24,7 @@ public class Tabs extends View {
 
     int iconColor;
     Path tabPath, iconPath;
-    Paint tabPaint, iconPaint;
+    Paint tabPaint, iconPaint, shadowPaint, borderPaint;
 
     public Tabs(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -29,7 +34,7 @@ public class Tabs extends View {
                 0, 0);
         try
         {
-            iconColor = a.getColor(R.styleable.Tabs_iconColor, Color.WHITE);
+            iconColor = a.getColor(R.styleable.Tabs_iconColor, Color.BLACK);
         }
         catch (Exception e)
         {
@@ -43,8 +48,14 @@ public class Tabs extends View {
     }
 
     private void init() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setOutlineProvider(new CustomOutline());
+        }
+
         tabPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         iconPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        shadowPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        borderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         tabPath = new Path();
         iconPath = new Path();
     }
@@ -60,17 +71,27 @@ public class Tabs extends View {
         super.onDraw(canvas);
 
         tabPaint.setAlpha(255);
-        tabPaint.setColor(Color.WHITE);
         tabPaint.setStyle(Paint.Style.FILL);
-
-        tabPath.addRoundRect(new RectF(0, 0, 600, 150),  100, 100, Path.Direction.CCW);
-        canvas.drawPath(tabPath, tabPaint);
-        canvas.clipPath(tabPath);
+        tabPaint.setColor(Color.WHITE);
+        canvas.drawRoundRect(new RectF(0, 0, 600, 150),  100, 100, tabPaint);
 
         iconPaint.setColor(Color.RED);
-        iconPath.addRoundRect(new RectF(0, 0, 150, 150),  100, 100, Path.Direction.CCW);
-        canvas.drawPath(iconPath, iconPaint);
-        canvas.clipPath(iconPath);
+        canvas.drawRoundRect(new RectF(0, 0, 150, 150),  100, 100, iconPaint);
 
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(1);
+        borderPaint.setColor(Color.BLACK);
+        borderPaint.setAlpha(100);
+        canvas.drawRoundRect(new RectF(0, 0, 600, 150),  100, 100, borderPaint);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private class CustomOutline extends ViewOutlineProvider {
+        @Override
+        public void getOutline(View view, Outline outline) {
+            outline.setRoundRect(new Rect(0,0,600, 200), 50);
+
+        }
     }
 }
